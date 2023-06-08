bf9e14f06bd5164889b14e5bd01a89e1a453e68f
igor4ikus <135968392+igor4ikus@users.noreply.github.com>
2023-06-08 14:03:42 +0300

    Add files via upload

---
+++ Company.java
@@ -0,0 +1,19 @@
+public class Company {
+    public String name;
+    public String email;
+    public String town;
+    public String occupation;
+    public int foundingDate;
+    public String founder;
+
+    public Company(String name, String email, String town, String occupation, int foundingDate, String founder){
+        this.name = name;
+        this.email = email;
+        this.town = town;
+        this.occupation = occupation;
+        this.foundingDate = foundingDate;
+        this.founder = founder;
+    }
+
+
+}
---
+++ Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+        Company company = new Company("Эирплейн","AirPlane_flights@gmail.com", "Москва", "сеть авиалиний ", 8);
+    }
+}
